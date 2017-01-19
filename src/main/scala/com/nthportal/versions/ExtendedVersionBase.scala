ddbc29b2eecd29bae8a12b40d87690df968ad35d
NthPortal <nthportal@gmail.com>
2017-01-19 15:46:06 -0500

    Remove fields from ExtendedVersionBase
    The fields don't seem to add anything to the API.

--- src/main/scala/com/nthportal/versions/ExtendedVersionBase.scala
+++ src/main/scala/com/nthportal/versions/ExtendedVersionBase.scala
@@ -1,7 +1,3 @@
 package com.nthportal.versions
 
-private[versions] trait ExtendedVersionBase[V <: VersionBase[V], E] {
-  val version: V
-  val extension: E
-  protected val extensionDef: ExtensionDef[E]
-}
+private[versions] trait ExtendedVersionBase[V <: VersionBase[V], E]
