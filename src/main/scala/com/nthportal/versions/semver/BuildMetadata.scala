2f7f1562f050af6def178b7c87462eb58bb940ca
NthPortal <nthportal@gmail.com>
2017-02-02 00:30:01 -0500

    Add missing Scaladocs

--- src/main/scala/com/nthportal/versions/semver/BuildMetadata.scala
+++ src/main/scala/com/nthportal/versions/semver/BuildMetadata.scala
@@ -3,11 +3,18 @@ package com.nthportal.versions.semver
 import com.nthportal.versions.semver._build_metadata._
 
 /**
-  * Metadata for a build may be appended to a
-  * [[http://semver.org/spec/v2.0.0.html#spec-item-10 SemVer version]].
+  * Utility object for build metadata parsers. (Metadata for a build may be
+  * appended to a [[http://semver.org/spec/v2.0.0.html#spec-item-10 SemVer version]].)
   */
 object BuildMetadata {
 
+  /**
+    * Returns a [[Parser]] for string build metadata.
+    *
+    * The parser uses the [[identity]] function.
+    *
+    * @return a parser for string build metadata
+    */
   implicit def stringMetadataParser: Parser[String] = _parser(identity)
 
   /**
