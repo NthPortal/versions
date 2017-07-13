ef08833f5d9bc45350f7d8000d4bb5d64e5381e1
NthPortal <nthportal@gmail.com>
2017-07-13 02:06:17 -0400

    Add implicit `+?` method

--- src/main/scala/com/nthportal/versions/semver/package.scala
+++ src/main/scala/com/nthportal/versions/semver/package.scala
@@ -261,7 +261,7 @@ package object semver {
       * @tparam M the type of the build metadata
       * @return a SemVer version with empty build metadata
       */
-    def withNoMetadata[M]: SemanticVersion[E, M] = SemanticVersion(ver, None)
+    def withNoMetadata[M]: SemanticVersion[E, M] = +?(None)
 
     /**
       * Creates a [[SemanticVersion SemVer version]] with the specified build
@@ -271,12 +271,22 @@ package object semver {
       * @tparam M the type of the build metadata
       * @return a SemVer version with the specified build metadata
       */
-    def withBuildMetadata[M](metadata: M): SemanticVersion[E, M] = SemanticVersion(ver, Some(metadata))
+    def withBuildMetadata[M](metadata: M): SemanticVersion[E, M] = +?(Some(metadata))
 
     /**
       * @see [[withBuildMetadata]]
       */
-    def +[M](buildMetadata: M): SemanticVersion[E, M] = withBuildMetadata(buildMetadata)
+    def +[M](buildMetadata: M): SemanticVersion[E, M] = +?(Some(buildMetadata))
+
+    /**
+      * Creates a [[SemanticVersion SemVer version]] with the specified build
+      * metadata [[Option]] from this extended version.
+      *
+      * @param buildMetadata an Option containing the build metadata
+      * @tparam M the type of the build metadata
+      * @return a SemVer version with the specified build metadata
+      */
+    def +?[M](buildMetadata: Option[M]): SemanticVersion[E, M] = SemanticVersion(ver, buildMetadata)
   }
 
 }
