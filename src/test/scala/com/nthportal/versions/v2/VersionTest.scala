c6e4664d0f244c05d2274c325465d6f63256f05d
NthPortal <nthportal@gmail.com>
2017-07-16 17:03:39 -0400

    Fix version parsing bug
    Fix bug where an invalid version ending in a '.' would be
    parsed as valid.

--- src/test/scala/com/nthportal/versions/v2/VersionTest.scala
+++ src/test/scala/com/nthportal/versions/v2/VersionTest.scala
@@ -34,6 +34,7 @@ class VersionTest extends SimpleSpec {
     Version parseVersion "1.3" shouldEqual Version(1)(3)
     Version parseVersion "0.0" shouldEqual Version(0)(0)
 
+    a [VersionFormatException] should be thrownBy {Version parseVersion "1.0."}
     a [VersionFormatException] should be thrownBy {Version parseVersion "-1.0"}
     a [VersionFormatException] should be thrownBy {Version parseVersion "1"}
     a [VersionFormatException] should be thrownBy {Version parseVersion "1.0.0"}
@@ -45,6 +46,7 @@ class VersionTest extends SimpleSpec {
     Version.parseAsOption("1.3").value shouldEqual Version(1)(3)
     Version.parseAsOption("0.0").value shouldEqual Version(0)(0)
 
+    Version parseAsOption "1.0." shouldBe empty
     Version parseAsOption "-1.0" shouldBe empty
     Version parseAsOption "1" shouldBe empty
     Version parseAsOption "1.0.0" shouldBe empty
